d063e8390814adc1b4c23dae7c0644ab7d7ece44
Bradley Spencer <rawwebdesign@gmail.com>
2025-07-23 22:49:49 -0400

    Speed Insights addition

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <SpeedInsights/>
     </html>
   )
 }
